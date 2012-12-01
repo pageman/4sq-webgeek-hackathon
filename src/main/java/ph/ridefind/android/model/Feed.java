7417923e7b17a00b9189767508e82efcddac0109
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-12-01 15:26:57 +0800

    Initial models
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

---
+++ src/main/java/ph/ridefind/android/model/Feed.java
@@ -0,0 +1,70 @@
+package ph.ridefind.android.model;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.Date;
+
+public class Feed {
+    private Long id;
+
+    private String desc;
+
+    @JsonProperty("image_url")
+    private String imageUrl;
+
+    private String link;
+
+    @JsonProperty("created_at")
+    private Date createdAt;
+
+    @JsonProperty("updated_at")
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ src/main/java/ph/ridefind/android/model/Tip.java
@@ -0,0 +1,80 @@
+package ph.ridefind.android.model;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.util.Date;
+
+public class Tip {
+    private Long id;
+
+    private String desc;
+
+    private String imageUrl;
+
+    @JsonProperty("fsq_id")
+    private String fsqId;
+
+    private int likes;
+
+    @JsonProperty("created_at")
+    private Date createdAt;
+
+    @JsonProperty("updated_at")
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getFsqId() {
+        return fsqId;
+    }
+
+    public void setFsqId(String fsqId) {
+        this.fsqId = fsqId;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
