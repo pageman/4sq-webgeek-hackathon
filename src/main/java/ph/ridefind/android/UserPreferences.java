89b92d519a565ad35c37500bc6a979e01dc4bfd0
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-12-01 11:28:26 +0800

    Created prefs class for saving the gcm id
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

---
+++ src/main/java/ph/ridefind/android/UserPreferences.java
@@ -0,0 +1,8 @@
+package ph.ridefind.android;
+
+import com.googlecode.androidannotations.annotations.sharedpreferences.SharedPref;
+
+@SharedPref(value = SharedPref.Scope.UNIQUE)
+public interface UserPreferences {
+    String registrationId();
+}
