792927eb4f00d7771cb1d0f49fa3da59b24bda55
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-12-01 16:30:43 +0800

    Properly populate feeds from the web service
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- src/main/java/ph/ridefind/android/activity/FeedListFragment.java
+++ src/main/java/ph/ridefind/android/activity/FeedListFragment.java
@@ -3,40 +3,57 @@ package ph.ridefind.android.activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import com.actionbarsherlock.app.SherlockFragment;
-import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Background;
+import com.googlecode.androidannotations.annotations.Bean;
 import com.googlecode.androidannotations.annotations.EFragment;
 import com.googlecode.androidannotations.annotations.ItemClick;
+import com.googlecode.androidannotations.annotations.UiThread;
 import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
 import ph.ridefind.android.R;
+import ph.ridefind.android.UserPreferences_;
 import ph.ridefind.android.adapter.FeedListAdapter;
 import ph.ridefind.android.model.Feed;
+import ph.ridefind.android.service.WebService;
+import ph.ridefind.android.service.WebServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @EFragment(R.layout.feed_list)
 public class FeedListFragment extends SherlockFragment {
+    private static final String TAG = FeedListFragment.class.getSimpleName();
     private FeedListAdapter adapter;
+
+    @Pref
+    UserPreferences_ preferences;
 
     @ViewById
     ListView list;
 
+    @Bean(WebServiceImpl.class)
+    WebService webService;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        List<Feed> feeds = new ArrayList<Feed>();
-        String url = "http://photos-b.ak.fbcdn.net/hphotos-ak-ash3/533540_380259988734115_1951594686_s.jpg";
-        feeds.add(Feed.buildFeed(getString(R.string.lorem), url));
-        feeds.add(Feed.buildFeed(getString(R.string.lorem), url));
-        feeds.add(Feed.buildFeed(getString(R.string.lorem), url));
-        feeds.add(Feed.buildFeed(getString(R.string.lorem), url));
-
-        adapter = new FeedListAdapter(getActivity(), feeds);
+        pullFeeds();
     }
 
-    @AfterViews
-    void afterViews() {
+    @Background
+    void pullFeeds() {
+        boolean exists = preferences.fsqId().exists();
+
+        if (!exists) return;
+
+        String fsqId = preferences.fsqId().get();
+
+        List<Feed> feeds = webService.getFeeds(fsqId);
+        showFeeds(feeds);
+    }
+
+    @UiThread
+    void showFeeds(List<Feed> feeds) {
+        adapter = new FeedListAdapter(getActivity(), feeds);
         list.setAdapter(adapter);
     }
 
