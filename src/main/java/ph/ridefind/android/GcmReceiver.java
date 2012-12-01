56d329c68935750b543170ed901c5280e62b68b3
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-12-01 12:00:19 +0800

    Use the annotated service :-/
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- src/main/java/ph/ridefind/android/GcmReceiver.java
+++ src/main/java/ph/ridefind/android/GcmReceiver.java
@@ -6,6 +6,7 @@ import com.google.android.gcm.GCMBroadcastReceiver;
 public class GcmReceiver extends GCMBroadcastReceiver {
     @Override
     protected String getGCMIntentServiceClassName(Context context) {
-        return "ph.ridefind.android.GcmIntentService";
+        return "ph.ridefind.android.GcmIntentService_";
     }
+
 }
