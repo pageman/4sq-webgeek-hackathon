6be45bab0dbcd1e8d74907b8feb706f3b5b9904c
Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>
2012-12-01 13:04:11 +0800

    Show a notification if the user checks in somewhere!
    Signed-off-by: Carlos Samuel Gavino <carlos.gavino@teamcodeflux.com>

--- src/main/java/ph/ridefind/android/GcmIntentService.java
+++ src/main/java/ph/ridefind/android/GcmIntentService.java
@@ -1,17 +1,25 @@
 package ph.ridefind.android;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import com.google.android.gcm.GCMBaseIntentService;
 import com.googlecode.androidannotations.annotations.EService;
 import com.googlecode.androidannotations.annotations.SystemService;
+import com.googlecode.androidannotations.annotations.res.StringRes;
 import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
+import ph.ridefind.android.activity.MainActivity_;
+
+import static java.text.MessageFormat.*;
 
 @EService
 public class GcmIntentService extends GCMBaseIntentService {
     private static String TAG = GcmIntentService.class.getSimpleName();
+    private static final int __ID = 1;
 
     @Pref
     UserPreferences_ preferences;
@@ -19,12 +27,39 @@ public class GcmIntentService extends GCMBaseIntentService {
     @SystemService
     NotificationManager notificationManager;
 
+    @StringRes
+    String title;
+
+    @StringRes
+    String info;
+
     protected GcmIntentService() {
         super(TAG);
     }
 
     @Override
     protected void onMessage(Context context, Intent intent) {
+        notify(intent.getExtras());
+    }
+
+    private void notify(Bundle bundle) {
+        Log.e(TAG, "Got this " + bundle);
+
+        int id = Integer.parseInt(bundle.getString("id"));
+        String name = bundle.getString("name");
+
+        // Instantiate notif
+        Notification notification = new Notification(R.drawable.icon, title, System.currentTimeMillis());
+        notification.defaults |= Notification.DEFAULT_LIGHTS;
+        notification.flags |= Notification.FLAG_AUTO_CANCEL | Notification.FLAG_SHOW_LIGHTS;
+
+        // Define message
+        Intent notificationIntent = new Intent(this, MainActivity_.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, PendingIntent.FLAG_ONE_SHOT);
+        notification.setLatestEventInfo(getApplicationContext(), title, format(info, name), contentIntent);
+
+        // Pass notif to manager
+        notificationManager.notify(__ID, notification);
     }
 
     @Override
